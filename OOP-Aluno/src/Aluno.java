25d0c7f6749a9bc3ecc11fdfa21eb0cc5d92cc38
Vinicius Gutierri <115495475+ViniciusGutierri@users.noreply.github.com>
2024-02-06 14:56:41 -0300

    Add files via upload

---
+++ OOP-Aluno/src/Aluno.java
@@ -0,0 +1,13 @@
+public class Aluno {
+    String nome;
+    int idade;
+    int matricula;
+
+    void exibeInformacoes() {
+        System.out.println("*****************************");
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Matrícula: " + matricula);
+        System.out.println("*****************************");
+    }
+}
---
+++ OOP-Aluno/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Aluno aluno = new Aluno();
+
+        aluno.nome = "Alfredo Junior";
+        aluno.idade = 23;
+        aluno.matricula = 123456789;
+
+        aluno.exibeInformacoes();
+    }
+}
