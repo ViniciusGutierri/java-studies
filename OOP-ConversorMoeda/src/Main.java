b0b12e74d966e1ceaee13d14fef512992caf4d6d
Vinicius Gutierri <115495475+ViniciusGutierri@users.noreply.github.com>
2024-02-19 14:14:54 -0300

    Add files via upload

---
+++ OOP-ConversorMoeda/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        ConversorMoeda conversorMoeda = new ConversorMoeda();
+        conversorMoeda.converterDolarParaReal(100);
+    }
+}
